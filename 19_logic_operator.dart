222a9a22c20855e2f1e73afeb244c6940de61bf6
Dishant Vithani <dishantvithani@gmail.com>
2025-09-06 13:29:31 +0530

    logical

---
+++ 19_logic_operator.dart
@@ -0,0 +1,27 @@
+// relational operator 
+import 'dart:io';
+void main()
+{
+    int a=0,b=0,c=0;
+    print(" Enter the value of A :");
+    a= int.parse(stdin.readLineSync().toString());
+
+    print(" Enter the value of B :");
+    b= int.parse(stdin.readLineSync().toString());
+
+    print(" Enter the value of C :");
+    c= int.parse(stdin.readLineSync().toString());
+
+    //use logical operator
+
+    bool result= a==b &&b==c;
+    print(" $result = $a==$b && $b==$c");
+
+    result = a<b || b<c;
+    print("$result =$a<$b || $b<$c");
+
+    result= !(a>100);
+    print("$result =!($a>100)");
+
+    
+}
