9305cb4280cf6d9fef0977e050082afd39731f92
dishantpatel10 <dishantvithani@gmail.com>
2025-09-08 16:02:34 +0530

    11

---
+++ 22_ternery_operator.dart
@@ -0,0 +1,10 @@
+//example of ternery operator
+import 'dart:io';
+void main()
+{
+    int time;
+    print(" Enter time in 24 hours format :");
+    time= int.parse(stdin.readLineSync().toStrint());
+
+    
+}
