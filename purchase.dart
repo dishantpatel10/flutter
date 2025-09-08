5eb4c181c6a5d76c3025292c9611c6208ec1a8dc
dishantpatel10 <dishantvithani@gmail.com>
2025-09-08 17:16:03 +0530

    purchase

---
+++ purchase.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main()
+{
+    int purchase=0,sales=0;
+
+    print(" Enter the total amount of purchase : ");
+    purchase= int.parse(stdin.readLineSync().toString());
+
+    print(" Enter the total amount of sales :");
+    sales= int.parse(stdin.readLineSync().toString());
+
+    print((purchase>sales) ? " YOU MAKE LOSS TODAY " : "YOU MAKE PROFIT TODAY ! CONGRATULATIONS ");
+}
