e3a197f62c5452c09ed892a13cc4c7c156c63248
dishantpatel10 <dishantvithani@gmail.com>
2025-09-08 16:35:04 +0530

    ternery

--- 22_ternery_operator.dart
+++ 22_ternery_operator.dart
@@ -4,7 +4,21 @@ void main()
 {
     int time;
     print(" Enter time in 24 hours format :");
-    time= int.parse(stdin.readLineSync().toStrint());
+    time= int.parse(stdin.readLineSync().toString());
 
-    
+    print(time);
+   /* if(time<12)
+    {
+        print("$time AM");
+    }
+    else
+    {
+        print((time-12).toString()+ "PM");
+    }*/
+
+    /*var output =(time<=12) ?"$time AM":(time-12).toString()+"PM";
+    print(output);*/
+
+    print((time<=12) ? "$time AM ":(time-12).toString()+ "PM");
+
 }
