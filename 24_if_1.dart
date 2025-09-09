c995360a953a35f47f2c79921e037d84863b9463
dishantpatel10 <dishantvithani@gmail.com>
2025-09-09 15:55:38 +0530

    enum

---
+++ 24_if_1.dart
@@ -0,0 +1,22 @@
+// business profit or losses 
+import 'dart:io';
+void main()
+{
+    int purchase=0,sales=0;
+
+    print(" Enter the total purchase price :");
+    purchase= int.parse(stdin.readLineSync().toString());
+
+    print(" Enter the total sales price :");
+    sales= int.parse(stdin.readLineSync().toString());
+
+    if(sales>purchase)
+    {
+        print(" Business man made a profit ");
+    }
+    if(sales<purchase)
+    {
+        print(" Business man made a losses");
+    }
+    print(" Thank you Good bye ");
+}
---
+++ 25_if_else_1.dart
@@ -0,0 +1,17 @@
+// eligibility gor PMVVY
+import 'dart:io';
+void main()
+{
+    int age;
+    print(" Enter your age :");
+    age= int.parse(stdin.readLineSync().toString());
+
+    if(age>=60)
+    {
+        print(" Congratulations , you are eligible for pension on investment amount @ 8 % interest rate ");
+    }
+    else
+    {
+        print(" You will become")
+    }
+}
