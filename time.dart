944dc63459fc9826d453339b197b04a9058e6f51
dishantpatel10 <dishantvithani@gmail.com>
2025-09-10 15:53:48 +0530

    time_condition

---
+++ time.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+void main()
+{
+    int time=0;
+    print(" Enter the time : ");
+    time= int.parse(stdin.readLineSync().toString());
+
+    if(time==1)
+    {
+        print(" The time is 1 AM ");
+    }
+    if(time==2)
+    {
+        print(" The time is 2 AM ");
+    }
+    if(time==3)
+    {
+        print(" The time is 3 AM ");
+    }
+    if(time==4)
+    {
+        print(" The time is 4 AM ");
+    }
+    if(time==5)
+    {
+        print(" The time is 5 AM ");
+    }
+    if(time==6)
+    {
+        print(" The time is 6 AM ");
+    }
+    if(time==7)
+    {
+        print(" The time is 7 AM ");
+    }
+    if(time==8)
+    {
+        print(" The time is 8 AM ");
+    }
+    if(time==9)
+    {
+        print(" The time is 9 AM ");
+    }
+    if(time==10)
+    {
+        print(" The time is 10 AM ");
+    }
+     if(time==11)
+    {
+        print(" The time is 11 AM ");
+    }
+    if(time==12)
+    {
+        print(" The time is 12 AM ");
+    }
+    if(time==13)
+    {
+        print(" The time is 1 PM ");
+    }
+    if(time==14)
+    {
+        print(" The time is 2 PM ");
+    }
+    if(time==15)
+    {
+        print(" The time is 3 PM ");
+    }
+    if(time==16)
+    {
+        print(" The time is 4 PM ");
+    }
+    if(time==17)
+    {
+        print(" The time is 5 PM ");
+    }
+    if(time==18)
+    {
+        print(" The time is 6 AM ");
+    }
+    if(time==19)
+    {
+        print(" The time is 7 PM ");
+    }
+    if(time==20)
+    {
+        print(" The time is 8 PM ");
+    }
+    if(time==21)
+    {
+        print(" The time is 9 PM ");
+    }
+    if(time==22)
+    {
+        print(" The time is 10 PM ");
+    }
+     if(time==23)
+    {
+        print(" The time is 11 PM ");
+    }
+    if(time==24)
+    {
+        print(" The time is 12 PM ");
+    }
+}
