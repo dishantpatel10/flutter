2a2b23f631c7269729ddb19df5b611ac3c985c17
dishantpatel10 <dishantvithani@gmail.com>
2025-10-02 12:50:39 +0530

    loop

---
+++ 35_for_loop_1.dart
@@ -0,0 +1,21 @@
+//wap to print multiplication table of given number 
+// input : number= 5
+// 5*1 =5
+// 5*2 =10
+
+import 'dart:io';
+void main()
+{
+    int number;
+    int answer;
+    int multiplier;
+
+    print(" ENTER THE NUMBER : ");
+    number =int.parse(stdin.readLineSync().toString());
+
+    for(multiplier =1;multiplier<=10;multiplier+1);
+    {
+        answer= number*multiplier;
+        print(" $number* $multiplier =$answer");
+    }
+}
